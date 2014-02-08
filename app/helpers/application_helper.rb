f4f577508053c9a185aa414d85bece6058cdf425
Michael Zalimeni <mzalimeni@gmail.com>
2014-02-08 08:26:42 -0500

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+   BASE_TITLE="LCC Online Directory"
+
+   #Returns the full title on a per-page basis
+   def full_title(page_title)
+      if page_title empty?
+        base_title
+      else
+        "#{base_title} | #{page_title}"
+      end
+   end
+
 end

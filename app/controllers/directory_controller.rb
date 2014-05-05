f72810e4827f4cc7f68f3c03e06b77f99417767a
Michael Zalimeni <mzalimeni@gmail.com>
2014-05-04 22:25:58 -0400

    Local time for application set to Eastern, and custom file name including current datetime for directory CSV download

--- app/controllers/directory_controller.rb
+++ app/controllers/directory_controller.rb
@@ -5,7 +5,7 @@ class DirectoryController < ApplicationController
   def index
     @users = User.all
     respond_to do |format|
-      format.csv { send_data @users.to_csv }
+      format.csv { send_data @users.to_csv, filename: 'lcc_directory_' + Time.current.to_s(:number) + '.csv' }
     end
   end
 
